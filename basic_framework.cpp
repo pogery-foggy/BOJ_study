317ff9edc11c05e3b13382164f7d860b0ae9279c
pogery_foggy <s2k616@gmail.com>
2023-03-07 00:30:30 +0900

    2

---
+++ basic_framework.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int answer;
+
+void init() { answer = 0; }
+
+void solve() {}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    init();
+    solve();
+    cout << answer << "\n";
+    return 0; // 정상종료시 반드시 0을 리턴해야합니다.
+}
