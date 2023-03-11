97efec8c401542ecc8f554eaf8fa047d28a4077b
pogery_foggy <s2k616@gmail.com>
2023-03-12 05:22:09 +0900

    0312

--- code/10951.cpp
+++ code/10951.cpp
@@ -5,4 +5,8 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+    int a, b;
+    while (!(cin >> a >> b).eof()) {
+        cout << a + b << "\n";
+    }
 }
