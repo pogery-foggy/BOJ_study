a6c6b11244a85000026bff06c912570bb4165991
pogery-foggy <s2k616@gmail.com>
2025-11-13 21:05:25 +0900

    1113

---
+++ code/2025/study/1106_dp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int dp[21][]
+void init() {}
+
+void solve() {}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    init();
+    solve();
+    return 0; // 정상종료시 반드시 0을 리턴해야합니다.
+}
